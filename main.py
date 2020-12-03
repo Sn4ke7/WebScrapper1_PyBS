3e1a643266e31056f555910d618f84bdac908db3
Filippo Dante Serpentis <dantes7m@gmail.com>
2020-12-03 23:54:36 +0100

    adding site URL

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
-    pass
+#URL of crawled page
+baseurl = 'https://www.responseelectronics.com'
+
+# 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+
+#if __name__ == '__main__':
+#    pass
