8d99e248619b7084ec8955af8b9b0bcd006f24a3
Filippo Dante Serpentis <dantes7m@gmail.com>
2020-12-07 23:50:36 +0100

    Some TODOs

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, 
 # r = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky')
 
 
-
 productlinks = []
-for x in range(1,2):
+for x in range(1,3):
     r = requests.get(f'https://www.thewhiskyexchange.com/c/35/japanese-whisky?pg={x}#productlist-filter')
     soup = BeautifulSoup(r.content, 'lxml')
 
@@ -28,6 +27,8 @@ print(productlinks)
 # print(productlist)
 
 # TODO: Find loop that would function as checker of page count (while with try except finally?)
+# TODO: Check if simplest form works with other sites
+# TODO: Experiment more with request and types of returned values
 
 # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
 
