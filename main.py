1b8b4c91dac268d6861079acebddc0a8e5121696
Filippo Dante Serpentis <dantes7m@gmail.com>
2020-12-14 22:46:33 +0100

    str - item , error fix

--- main.py
+++ main.py
@@ -5,27 +5,34 @@ from bs4 import BeautifulSoup
 #baseurl = 'https://www.responseelectronics.com'
 baseurl = 'https://www.thewhiskyexchange.com'
 
-headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-
+#required for being able to pull out page response (some pages block request if it's not acting as user)
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+}
+# binding request from page to variable
 # r = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky')
 
-
 productlinks = []
+
 for x in range(1,3):
     r = requests.get(f'https://www.thewhiskyexchange.com/c/35/japanese-whisky?pg={x}#productlist-filter')
     soup = BeautifulSoup(r.content, 'lxml')
-
     productlist = soup.find_all('div', class_='item')
-
     for item in productlist:
         for link in item.find_all('a', href=True):
             productlinks.append(baseurl + link['href'])
-            # print(link['href'])
+           #print(link['href'])
 
-testlink = "https://www.thewhiskyexchange.com/p/37325/suntory-torys-classic"
+testlink = 'https://www.thewhiskyexchange.com/p/37325/suntory-torys-classic'
 r = requests.get(testlink, headers=headers)
-soup = BeautifulSoup(r.content, 'lxml')
-name = print(soup.find('h1'), class_="product-main__name").text.strip()
+#soup = BeautifulSoup(r.content, 'lxml')
+#name = print(soup.find('h1'), class_="product-main__name").text.strip()
+
+
+# TODO: Find loop that would function as checker of page count (while with try except finally?)
+# TODO: Check if simplest form works with other sites
+# TODO: Experiment more with request and types of returned values
+
 
 # name = soup.find('h1', class_='product-main__name').text.strip()
 # rating = soup.find('span', class_="review-overview__rating star-rating start-rating--30").text
@@ -34,13 +41,6 @@ name = print(soup.find('h1'), class_="product-main__name").text.strip()
 # print(len(productlinks))
 # print(productlinks)
 # print(productlist)
-
-# TODO: Find loop that would function as checker of page count (while with try except finally?)
-# TODO: Check if simplest form works with other sites
-# TODO: Experiment more with request and types of returned values
-
-# 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-
 
 #if __name__ == '__main__':
 #    pass
